7debc63d258cf8df75f47f677f1a59f8e61557f4
Rakshit Midha <rakshit_midha@yahoo.com>
2025-01-22 03:32:58 +0100

    add docker file and docker compose

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    host: '0.0.0.0', // Allow the server to be accessible outside the container
+    port: 5173, // Ensure it's using the port you expect
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
